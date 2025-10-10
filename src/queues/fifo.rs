85854bf63cafbae862605efd5a348ba402b0080c
CameronJYoung <cameronjyoung123@gmail.com>
2025-10-10 21:08:13 +0000

    Feat: Adds method to get next sequence item

--- src/queues/fifo.rs
+++ src/queues/fifo.rs
@@ -3,7 +3,7 @@ use std::collections::VecDeque;
 use crate::core::{
     config::QueueConfig,
     message::Message,
-    queue::{Queue, QueueInputError, QueueOutputError},
+    queue::{Queue, QueueInputError, QueueOutputError, QueueSequenceCalcError},
 };
 
 #[derive(Default)]
@@ -47,6 +47,19 @@ impl<T: Send> Queue<T> for FifoQueue<T> {
         match out_item {
             Some(i) => Ok(i),
             None => Err(QueueOutputError::Empty),
+        }
+    }
+
+    async fn get_next_sequence_count(&mut self) -> Result<u64, QueueSequenceCalcError> {
+        if self.queue_config.max_size.is_some_and(|m| self.len() == m) {
+            return Err(QueueSequenceCalcError::MaxSize);
+        }
+
+        let final_item = self.items.front();
+
+        match final_item {
+            Some(i) => Ok(i.sequence_count + 1),
+            None => Ok(1),
         }
     }
 }
@@ -152,5 +165,32 @@ mod tests {
         let out = queue.output().await;
 
         assert_eq!(out, Err(QueueOutputError::Empty));
+    }
+
+    #[tokio::test]
+    async fn test_get_next_sequence_count_max_size() {
+        let queue_config = QueueConfig { max_size: Some(1) };
+        let mut queue: FifoQueue<String> = FifoQueue::new(queue_config);
+
+        let message = Message::new("test_id".to_string(), 1, 2, "test".to_string());
+
+        queue.input(message).await.unwrap();
+
+        let next_seq = queue.get_next_sequence_count().await;
+
+        assert_eq!(next_seq, Err(QueueSequenceCalcError::MaxSize));
+    }
+
+    #[tokio::test]
+    async fn test_get_next_sequence_count() {
+        let mut queue: FifoQueue<String> = FifoQueue::default();
+
+        let message = Message::new("test_id".to_string(), 1, 1, "test".to_string());
+
+        queue.input(message).await.unwrap();
+
+        let next_seq = queue.get_next_sequence_count().await;
+
+        assert_eq!(next_seq, Ok(2));
     }
 }
